e4ea3cf802577e94d0cde69a156323cc7be5edb9
tyler <tyler@cruftbusters.com>
2021-10-02 15:38:10 -0500

    extract screen projector

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,43 @@ function lookupRgb(value: string) {
   }
 }
 
+interface LatLon {
+  lat: number
+  lon: number
+}
+
+class ScreenProjector {
+  left: number = 0
+  right: number = 0
+  top: number = 0
+  bottom: number = 0
+
+  static fromLatLons(latLons: Array<LatLon>) {
+    const lons = latLons.map((it) => it.lon)
+    const lats = latLons.map((it) => it.lat)
+    const screenProjector = new ScreenProjector()
+    screenProjector.left = Math.min(...lons)
+    screenProjector.right = Math.max(...lons)
+    screenProjector.top = Math.max(...lats)
+    screenProjector.bottom = Math.min(...lats)
+    return screenProjector
+  }
+
+  screenPointToCoordinatePoint(x: number, y: number) {
+    return [
+      (x / window.innerWidth) * (this.right - this.left) + this.left,
+      (-y / window.innerHeight) * (this.top - this.bottom) + this.top,
+    ]
+  }
+
+  coordinatePointToScreenPoint(a: number, b: number) {
+    return [
+      ((a - this.left) / (this.right - this.left)) * window.innerWidth,
+      ((-b + this.top) / (this.top - this.bottom)) * window.innerHeight,
+    ]
+  }
+}
+
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
   useEffect(() => {
@@ -36,21 +73,18 @@ function App() {
     const context = canvas.getContext('2d')!
     context.fillStyle = '#004400'
 
-    const lons = snapshot.content.locations.map((it) => it.lon)
-    const lats = snapshot.content.locations.map((it) => it.lat)
-    const left = Math.min(...lons)
-    const right = Math.max(...lons)
-    const top = Math.max(...lats)
-    const bottom = Math.min(...lats)
+    const screenProjector = ScreenProjector.fromLatLons(
+      snapshot.content.locations,
+    )
+
     const pixelSize = 5
     for (let x = 0; x < window.innerWidth; x += pixelSize) {
       for (let y = 0; y < window.innerHeight; y += pixelSize) {
-        const [nearest] = geokdbush.around(
-          index,
-          (x / window.innerWidth) * (right - left) + left,
-          (-y / window.innerHeight) * (top - bottom) + top,
-          1,
+        const [lon, lat] = screenProjector.screenPointToCoordinatePoint(
+          x,
+          y,
         )
+        const [nearest] = geokdbush.around(index, lon, lat, 1)
         context.fillStyle = lookupRgb(nearest.leaves)!
         context.fillRect(x, y, pixelSize, pixelSize)
       }
@@ -58,13 +92,8 @@ function App() {
 
     snapshot.content.locations.forEach(({ lat, lon }) => {
       context.beginPath()
-      context.arc(
-        ((lon - left) / (right - left)) * window.innerWidth,
-        ((-lat + top) / (top - bottom)) * window.innerHeight,
-        5,
-        0,
-        2 * Math.PI,
-      )
+      const [x, y] = screenProjector.coordinatePointToScreenPoint(lon, lat)
+      context.arc(x, y, 5, 0, 2 * Math.PI)
       context.stroke()
     })
   }, [canvasRef])
