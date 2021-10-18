b9b74114b37458637bf2cfed40952b9f7292bfee
tyler <tyler@cruftbusters.com>
2021-10-18 04:16:29 -0500

    memoize pane initializer callback

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,21 @@ import { BaseLayer } from './layer/BaseLayer'
 import { Pane } from './usePane'
 import { Vector2 } from './Types'
 import { paneStateFromPoints } from './PaneUtils'
+import { useCallback } from 'react'
 
 function App() {
+  const getInitialPaneState = useCallback(
+    (size: Vector2) =>
+      paneStateFromPoints(
+        size,
+        snapshot.features,
+        (it) => it.geometry.coordinates[0],
+        (it) => it.geometry.coordinates[1],
+      ),
+    [],
+  )
   return (
-    <Pane
-      getInitialPaneState={(size: Vector2) =>
-        paneStateFromPoints(
-          size,
-          snapshot.features,
-          (it) => it.geometry.coordinates[0],
-          (it) => it.geometry.coordinates[1],
-        )
-      }
-    >
+    <Pane getInitialPaneState={getInitialPaneState}>
       <BaseLayer />
       <FallLayer />
       <StationLayer />
